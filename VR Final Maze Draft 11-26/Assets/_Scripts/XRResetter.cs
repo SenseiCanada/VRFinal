5b4346bb10c98cc1765eef1e31678c9fc9f0de20
SenseiCanada <117216802+SenseiCanada@users.noreply.github.com>
2023-12-06 21:18:17 -0500

    added basic lava death

---
+++ VR Final Maze Draft 11-26/Assets/_Scripts/XRResetter.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class XRResetter : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 startPosition;
+    public bool ResetEntireScene;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LavaNotifier.OnFallInLava += ResetPosition;
+        LavaNotifier.OnFallInLava += ResetScene;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ResetPosition(GameObject xrRig)
+    {
+        xrRig.transform.position = startPosition;
+    }
+
+    private void ResetScene(GameObject xrRig)
+    {
+        if (ResetEntireScene)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+    }
+}
