2c6e5afad7a0c77d2e41d164b53a102faaf2a4db
SenseiCanada <117216802+SenseiCanada@users.noreply.github.com>
2023-12-12 06:59:10 -0500

    trying to fix floor crushers

--- VR Final Maze Draft 11-26/Assets/_Scripts/FloorCrusherMover.cs
+++ VR Final Maze Draft 11-26/Assets/_Scripts/FloorCrusherMover.cs
@@ -5,47 +5,42 @@ using System;
 
 public class FloorCrusherMover : MonoBehaviour
 {
-    public GameObject topPoint; // The starting point
-    public GameObject bottomPoint; // The ending point
-    public Transform topTransform;
-    public Transform bottomTransform;
-    public float speed = 2f; // Speed of movement
-
-    private Vector3 currentTarget;
+       public List<Transform> points;
+    public Transform platform;
+    int goalPoint = 0;
+    [SerializeField] float moveSpeed = 2;
 
     public static event Action<GameObject> OnCrusherCollide;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        topPoint = GameObject.Find("TopPoint");
-        bottomPoint = GameObject.Find("BottomPoint");
-
-        topTransform = topPoint.transform;
-        bottomTransform = bottomPoint.transform;
-
-        SetTarget(topTransform.position);
         
-}
-
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
-
-        // If the platform reaches the target, set a new target
-        if (Vector3.Distance(transform.position, currentTarget) < 0.01f)
-        {
-            if (currentTarget == topTransform.position)
-                SetTarget(bottomTransform.position);
-            else SetTarget(topTransform.position);
-
-        }
+        MoveToNextPoint();
     }
 
-    void SetTarget(Vector3 newTarget)
+    void MoveToNextPoint()
     {
-        currentTarget = newTarget;
+        platform.position = Vector3.MoveTowards(platform.position, points[goalPoint].position, Time.deltaTime * moveSpeed);
+
+        if (Vector3.Distance(platform.position, points[goalPoint].position) < 0.1f)
+        {
+            if (goalPoint == points.Count - 1)
+            {
+                goalPoint = 0;
+            }
+            else
+            {
+                goalPoint++;
+            }
+        }
+
     }
+
+    
 
     private void OnTriggerEnter(Collider other)
     {
